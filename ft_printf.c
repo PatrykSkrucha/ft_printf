ba55fdb5c6636a29b2227777d29c47d7a32e63a5
PatrykSkrucha <patryk.skrucha@gmail.com>
2022-11-01 14:11:47 +0100

    hex work

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:41:01 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/11/01 12:07:35 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/11/01 14:11:32 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "../libft/libft.h"
+
+static int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
 
 static int	ft_putnbr(int n)
 {
-	char	a;
-	int counter;
+	int	counter;
 
 	counter = 0;
 	if (n == -2147483648)
@@ -29,9 +35,8 @@ static int	ft_putnbr(int n)
 	}
 	else if (n < 0)
 	{
-		write(1, "-", 1);
+		counter += ft_putchar('-');
 		counter += ft_putnbr(-n);
-		counter++;
 	}
 	else if (n > 9)
 	{
@@ -40,93 +45,72 @@ static int	ft_putnbr(int n)
 	}
 	else
 	{
-		a = n + '0';
-		write(1, &a, 1);
+		ft_putchar(n + '0');
 		counter++;
 	}
 	return (counter);
 }
 
-static int	ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
-
 static int	ft_putstr(char *s, int control)
 {
-	int len;
+	int	len;
+
 	if (s == NULL)
 	{
 		write(1, "(null)", 6);
-		if(control)
+		if (control)
 			free(s);
 		return (6);
 	}
-
-	len = (int)strlen(s); //zmienic to
+	len = (int)ft_strlen(s); //zmienic to
 	if (s)
 		write(1, s, len);
-	if(control)
+	if (control)
 		free(s);
 	return (len);
 }
 
-
-static char *to_hex(long long int a, int control, int pointer)
+static int	to_hex(long long int a, int control)
 {
-	char *str;
-	int mem_size;
-	long long int temp;
+	int	counter;
 
-	temp = a;
-	mem_size = 0;
-	while (temp >= 1)
+	counter = 0;
+	if (a > 16)
 	{
-		temp /= 16;
-		mem_size++;
+		counter += to_hex(a / 16, control);
+		counter += to_hex(a % 16, control);
 	}
-	if (pointer)
-		mem_size += 2;
-	str = (char*)malloc(sizeof(char) * (mem_size + 1));
-	if (!str)
-		return (NULL);
-	str[mem_size] = '\0';
-	while (a >= 1)
+	else if (a < 10)
 	{
-		mem_size--;
-		temp = a % 16;
-		if (temp < 10)
-			str[mem_size] = temp + '0';
-		else
-		{
+		counter += ft_putchar(a + '0');
+	}
+	else	
+	{
 			if (control)
-				str[mem_size] = temp + '7';
+				counter += ft_putchar(a + '7');
 			else
-				str[mem_size] = temp + 'W';
+				counter += ft_putchar(a + 'W');
 		}
-		a /= 16;
-	}
-	return (str);
+	return (counter);
 }
 
-static char	*create_pointer(long long number)
-{
-	char	*str;
-	void	*ptr;
 
-	ptr = (void*)number;
-	if (ptr == NULL)
-		{
-			str = malloc(3);
-			str = "0x0";
-			return (str);
-		}
-	str = to_hex(number, 0, 1);
-	str[1] = 'x';
-	str[0] = '0';
-	return (str);
-}
+// static int	create_pointer(long long number)
+// {
+// 	int		counter;
+// 	void	*ptr;
+
+// 	ptr = (void*)number;
+// 	if (ptr == NULL)
+// 		{
+// 			write(1, "0x0", 3);
+// 			return (3);
+// 		}
+// 	str = to_hex(number, 0, 1);
+// 	str[1] = 'x';
+// 	str[0] = '0';
+// 	return (str);
+// }
 
 static int	print_unsigned(unsigned int n)
 {
@@ -169,15 +153,15 @@ int ft_printf(const char *format, ...)
 			else if (format[a + 1] == 's')
 				result += ft_putstr(va_arg(args, char *), 0);
 			else if (format[a + 1] == 'x')
-				result += ft_putstr(to_hex(va_arg(args, long long int), 0, 0), 1);	
+				result += to_hex(va_arg(args, long long int), 0);	
 			else if (format[a + 1] == 'X')
-				result += ft_putstr(to_hex(va_arg(args, long long int), 1, 0), 1);
+				result += to_hex(va_arg(args, long long int), 1);
 			else if (format[a + 1] == '%')
 				result += ft_putchar('%');
 			else if (format[a + 1] == 'u')
 				result += print_unsigned(va_arg(args, unsigned int));
-			else if (format[a + 1] == 'p')
-				result += ft_putstr(create_pointer(va_arg(args, long long int)), 1);
+			// else if (format[a + 1] == 'p')
+			// 	result += ft_putstr(create_pointer(va_arg(args, long long int)), 1);
 			a += 2;
 		}
 		else
@@ -192,13 +176,14 @@ int ft_printf(const char *format, ...)
 
 int main()
 {
-	int b = 12548;
+	int b = 100;
 	int *c = &b;
 	char *s = NULL;
-	// ft_printf("%s", (char *)NULL);
+	// printf("%i",printf("%p", (void*)NULL));
 	
-
-	ft_printf("%x", 0);
+// 
+	int a = ft_printf("hello%i", b);
+	ft_printf("size: 8??? %i", a);
 	// printf("%p\n", c);
 	// printf("%p", &c);
 	// printf("size: %i", a);
