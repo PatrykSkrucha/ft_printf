811af204960d705c40c39c698c87c0e5cd8ecdf8
PatrykSkrucha <patryk.skrucha@gmail.com>
2022-11-01 16:51:23 +0100

    fixed

---
+++ printers.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 16:34:53 by pskrucha          #+#    #+#             */
+/*   Updated: 2022/11/01 16:37:35 by pskrucha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putnbr(int n)
+{
+	int	counter;
+
+	counter = 0;
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	else if (n < 0)
+	{
+		counter += ft_putchar('-');
+		counter += ft_putnbr(-n);
+	}
+	else if (n > 9)
+	{
+		counter += ft_putnbr(n / 10);
+		counter += ft_putnbr(n % 10);
+	}
+	else
+	{
+		ft_putchar(n + '0');
+		counter++;
+	}
+	return (counter);
+}
+
+int	ft_putstr(char *s)
+{
+	int	len;
+
+	len = 0;
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (s[len])
+		len++;
+	if (s)
+		write(1, s, len);
+	return (len);
+}
